db1c04c7b47a13dfdf2c097dbe5444d4b3db9716
95Donguk <midgard95@nate.com>
2024-03-09 19:29:34 +0900

    git init

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/MzdInterviewAssignmentApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mz.mzdinterviewassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzdInterviewAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzdInterviewAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/mz/mzdinterviewassignment/MzdInterviewAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mz.mzdinterviewassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzdInterviewAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
