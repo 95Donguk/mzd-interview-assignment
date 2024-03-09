992a5dfab73e80c3ebbbc092ed5ab6ecf6505709
95Donguk <midgard95@nate.com>
2024-03-10 02:56:59 +0900

    feat: 생성일시, 수정일시 공통 컬럼을 담은 BaseEntity 구현

--- src/main/java/kr/co/mz/mzdinterviewassignment/MzdInterviewAssignmentApplication.java
+++ src/main/java/kr/co/mz/mzdinterviewassignment/MzdInterviewAssignmentApplication.java
@@ -2,8 +2,10 @@ package kr.co.mz.mzdinterviewassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MzdInterviewAssignmentApplication {
 
     public static void main(String[] args) {
