255ccb85cdfead8b43b3de879c367eef22667408
95Donguk <midgard95@nate.com>
2024-03-11 01:52:41 +0900

    feat: 프로필 생성 요청 DTO CreateProfilePequest 구현

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/dto/request/profile/CreateProfileRequest.java
@@ -0,0 +1,37 @@
+package kr.co.mz.mzdinterviewassignment.dto.request.profile;
+
+import jakarta.validation.constraints.Pattern;
+import kr.co.mz.mzdinterviewassignment.domain.member.Member;
+import kr.co.mz.mzdinterviewassignment.domain.profile.Profile;
+import kr.co.mz.mzdinterviewassignment.domain.profile.ProfileStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateProfileRequest {
+
+    @Pattern(
+        regexp = "^[가-힣a-zA-Z0-9-_]{2,8}$",
+        message = "닉네임은 영문 대소문자, 숫자, 한글로 구성된 2 ~ 8자리로 입력해주세요."
+    )
+    private String nickname;
+
+    @Pattern(regexp = "^01 (?:0|1| [6-9]) (?:\\d {3}|\\d {4}) \\d {4}$",
+        message = "휴대전화 번호는 하이픈(-)을 제외한 10자리 또는 11자리로 입력해주세요.")
+    private String phoneNumber;
+
+    private String address;
+
+    public Profile toEntity(final ProfileStatus profileStatus,
+                            final Member member) {
+        return Profile.builder()
+            .nickname(nickname)
+            .phoneNumber(phoneNumber)
+            .address(address)
+            .profileStatus(profileStatus)
+            .member(member)
+            .build();
+    }
+}
