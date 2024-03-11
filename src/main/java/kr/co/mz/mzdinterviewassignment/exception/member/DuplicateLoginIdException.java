12bc0c67f5a0a837909e45ded0e689e1c8685bc8
95Donguk <midgard95@nate.com>
2024-03-12 00:44:38 +0900

    feat: 중복 아이디 에러 구현

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/exception/member/DuplicateLoginIdException.java
@@ -0,0 +1,10 @@
+package kr.co.mz.mzdinterviewassignment.exception.member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DuplicateLoginIdException extends RuntimeException {
+    public DuplicateLoginIdException(final String loginId) {
+        log.error("이미 존재하는 아이디입니다. 아이디: {}", loginId);
+    }
+}
