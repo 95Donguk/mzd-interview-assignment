814a6fbdc7fff8de2bc1fb96ddbf4c6165216721
95Donguk <midgard95@nate.com>
2024-03-12 00:25:47 +0900

    refactor: 회원 상태 에러 코드 제거

--- src/main/java/kr/co/mz/mzdinterviewassignment/exception/status/StatusExceptionCode.java
+++ src/main/java/kr/co/mz/mzdinterviewassignment/exception/status/StatusExceptionCode.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum StatusExceptionCode {
-    INVALID_MEMBER("유효하지 않은 회원 상태입니다."),
     INVALID_PROFILE("유효하지 않은 프로필 상태입니다.");
 
     private final String message;
 
-    StatusExceptionCode(String message) {
+    StatusExceptionCode(final String message) {
         this.message = message;
     }
 }
