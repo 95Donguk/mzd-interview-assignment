8d09c29c9f3cc164435aaa6da35afa7a670963d6
95Donguk <midgard95@nate.com>
2024-03-12 00:46:25 +0900

    feat: 빈 프로필 에러 구현

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/exception/profile/EmptyProfileException.java
@@ -0,0 +1,10 @@
+package kr.co.mz.mzdinterviewassignment.exception.profile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EmptyProfileException extends RuntimeException {
+    public EmptyProfileException(final Long memberNo) {
+        log.error("{} 회원 식별번호의 프로필이 없습니다. 프로필을 추가해야 합니다.", memberNo);
+    }
+}
