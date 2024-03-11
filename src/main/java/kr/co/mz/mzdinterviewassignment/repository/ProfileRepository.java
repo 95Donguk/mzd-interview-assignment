b2de69f345a8dee6ef29ff1079cae5d58cf9529a
95Donguk <midgard95@nate.com>
2024-03-12 03:27:03 +0900

    feat: Profile JpaRepository 구현

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/repository/ProfileRepository.java
@@ -0,0 +1,16 @@
+package kr.co.mz.mzdinterviewassignment.repository;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+import kr.co.mz.mzdinterviewassignment.domain.member.Member;
+import kr.co.mz.mzdinterviewassignment.domain.profile.Profile;
+import kr.co.mz.mzdinterviewassignment.domain.profile.ProfileStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findProfileByMemberAndProfileStatus(final Member member,
+                                                          final ProfileStatus profileStatus);
+
+    List<Profile> findAllByMember(final Member member);
+}
