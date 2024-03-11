e34684ae8a9f37b12211bc909a82016526e0e360
95Donguk <midgard95@nate.com>
2024-03-12 00:47:07 +0900

    feat: 회원 식별 번호와 프로필의 회원 식별 번호 불일치 에러

---
+++ src/main/java/kr/co/mz/mzdinterviewassignment/exception/profile/NonMatchMemberNoException.java
@@ -0,0 +1,10 @@
+package kr.co.mz.mzdinterviewassignment.exception.profile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NonMatchMemberNoException extends RuntimeException {
+    public NonMatchMemberNoException(final Long memberNo, final Long profileMemberNo) {
+        log.error("회원 식별 번호와 프로필의 회원 식별 번호가 다릅니다. 회원 식별 번호 :{}, 프로필의 회원 식별 번호 : {}", memberNo, profileMemberNo);
+    }
+}
