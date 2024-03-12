cf6b802e8a2183fd53574f7f087b58f380112c8c
95Donguk <midgard95@nate.com>
2024-03-12 22:43:21 +0900

    refactor: request DTO 정규 표현식 변경 및 @NotNull 추가

--- src/main/java/kr/co/mz/mzdinterviewassignment/dto/request/member/CreateMemberRequest.java
+++ src/main/java/kr/co/mz/mzdinterviewassignment/dto/request/member/CreateMemberRequest.java
@@ -1,5 +1,6 @@
 package kr.co.mz.mzdinterviewassignment.dto.request.member;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import kr.co.mz.mzdinterviewassignment.domain.member.Member;
 import lombok.AccessLevel;
@@ -14,13 +15,16 @@ public class CreateMemberRequest {
 
     @Pattern(regexp = "^[a-z\\d]{4,20}$",
         message = "아이디는 영문 소문자와 숫자로 구성된 4 ~ 12자리로 입력해주세요.")
+    @NotNull(message = "로그인 아이디는 필수입니다")
     private String loginId;
 
     @Pattern(regexp = "^[가-힣]{2,8}$", message = "이름은 한글로 구성된 2 ~ 8자리로 입력해주세요.")
+    @NotNull(message = "이름은 필수입니다")
     private String name;
 
     @Pattern(regexp = "^[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
         message = "비밀번호는 영문 대소문자, 숫자, 특수문자'~!@#$%^&*()+|='로 구성된 8 ~ 16자리로 입력해주세요.")
+    @NotNull(message = "비밀번호는 필수입니다")
     private String password;
 
     public Member toEntity() {
