939380f0c1dcb053b7ef1c5711503a4a59aa7da8
95Donguk <midgard95@nate.com>
2024-03-12 17:22:12 +0900

    style: 빈 프로필 에러 메시지 포맷 수정

--- src/main/java/kr/co/mz/mzdinterviewassignment/exception/profile/EmptyProfileException.java
+++ src/main/java/kr/co/mz/mzdinterviewassignment/exception/profile/EmptyProfileException.java
@@ -5,6 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EmptyProfileException extends RuntimeException {
     public EmptyProfileException(final Long memberNo) {
-        log.error("{} 회원 식별번호의 프로필이 없습니다. 프로필을 추가해야 합니다.", memberNo);
+        log.error("회원 식별번호 {} 의 프로필이 없습니다. 프로필을 추가해야 합니다.", memberNo);
     }
 }
