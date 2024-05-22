7f3613e7ac8f15f3a849eb60bec6f7701f340de9
kimdonguk <donguk@mz.co.kr>
2024-05-22 18:32:35 +0900

    refactor: CreateProfileRequest 레코드 클래스로 전환

--- src/main/java/kr/co/mz/mzdinterviewassignment/dto/request/profile/CreateProfileRequest.java
+++ src/main/java/kr/co/mz/mzdinterviewassignment/dto/request/profile/CreateProfileRequest.java
@@ -10,33 +10,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-public class CreateProfileRequest {
-
+public record CreateProfileRequest(
     @Pattern(
         regexp = "^[가-힣a-zA-Z0-9-_]{2,8}$",
         message = "닉네임은 영문 대소문자, 숫자, 한글로 구성된 2 ~ 8자리로 입력해주세요."
     )
     @NotNull(message = "별명은 필수입니다")
-    private String nickname;
+    String nickname,
 
     @Pattern(regexp = "^01(?:0|1|[6-9])(?:\\d{7}|\\d{8})$",
         message = "휴대전화 번호는 하이픈(-)을 제외한 10자리 또는 11자리로 입력해주세요.")
     @NotNull(message = "휴대전화 번호는 필수입니다")
-    private String phoneNumber;
+    String phoneNumber,
 
-    private String address;
-
-    public Profile toEntity(final ProfileStatus profileStatus,
-                            final Member member) {
-        return Profile.builder()
-            .nickname(nickname)
-            .phoneNumber(phoneNumber)
-            .address(address)
-            .profileStatus(profileStatus)
-            .member(member)
-            .build();
-    }
+    String address
+) {
+  public Profile toEntity(final ProfileStatus profileStatus,
+      final Member member) {
+    return Profile.builder()
+        .nickname(nickname)
+        .phoneNumber(phoneNumber)
+        .address(address)
+        .profileStatus(profileStatus)
+        .member(member)
+        .build();
+  }
 }
